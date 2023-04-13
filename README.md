0381921e0a4bb790517ccd393c250172d6c6877d
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-04-14 00:40:11 +0100

    image upload script

--- README.md
+++ README.md
@@ -2,7 +2,23 @@
 
 [Weaviate](https://weaviate.io/) is a [vector database](https://learn.microsoft.com/en-us/semantic-kernel/concepts-ai/vectordb) that allows you to create and query embeddings with pre—trained deep learning models. It integrates with ResNet—50 to vectorize images, making it possible to build an image similarity search engine with relative ease.
 
+
+Set up for use:
 ```
 npm install
 docker-compose up -d
+```
+Create a JSON file from the provide JSON URL:
+```
+node util/jsonScrape.js https://api.imgflip.com/get_memes
+```
+
+Scrape JSON file for all images into a new directory:
+```
+node util/imageScrape.js
+```
+Upload the images in the new directory into the [Weaviate](https://weaviate.io/) vector database ready to be searched:
+
+```
+node util/imgUpload.js
 ```
--- util/imgUpload.js
+++ util/imgUpload.js
@@ -3,31 +3,33 @@ import fs from 'fs'
 const filepath = './memes/';
 
 
-fs.readdir(filepath, 'utf-8')
-  .then(data => {
+fs.readdir(filepath,(err, data) => {
+  if (err)
+    console.log(err);
+  else {
+    console.log("\nCurrent directory filenames:");
+      data.map(item => {
+          let dir = filepath+item;
+          //console.log(item)
+          upload(dir,item)
 
-    data.map(item => {
-        // console.log(filepath+item)
-        let dir = filepath+item;
-        upload(dir, item)
-      });
+        });
+  
 
-    // console.log(filepath+data);
-  });
+  }});
 
 async function upload(filePath, fileName) {
-     const img = fs.readFileSync(filePath, );
-    // const fileName = filename
-    // const b64 = Buffer.from(img).toString('base64');
+    const img = fs.readFileSync(filePath);
+    const b64 = Buffer.from(img).toString('base64');
 
     console.log(filePath, fileName);
 
-    // await client.data.creator()
-    //     .withClassName('Meme')
-    //     .withProperties({
-    //         image: b64,
-    //         text: fileName
-    //     })
-    //     .do();
+    await client.data.creator()
+        .withClassName('Meme')
+        .withProperties({
+            image: b64,
+            text: fileName
+        })
+        .do();
 
 }
